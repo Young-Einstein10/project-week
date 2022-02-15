ea4392bed135e561e212f7d577330f552a4aa1ab
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-02-15 12:51:06 +0100

    Implement Adding and Updating a Todo

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 const form = document.querySelector("form");
 const todosInput = document.querySelector(".enter-todos-input");
 const todosWrapper = document.querySelector(".todos-wrapper");
-
-const todosList = [
-  {
-    id: 1,
-    todo: "A new todo",
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    todo: "Second Todo",
-    isCompleted: false,
-  },
-];
+const numOfTodosLeft = document.querySelector(".no-of-items-left");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -24,20 +12,132 @@ form.addEventListener("submit", (event) => {
   clearTodosInput();
 });
 
-const addNewTodo = (value) => {
-  const listItem = `
-        <li class="todo-item">
-            <div class="rounded-corner">
-                <input class="checkbox" type="checkbox" />
-                <span class="check"></span>
-            </div>
-            <p class="todo">${value}</p>
-        </li>
-        `;
+function getTodosList() {
+  return JSON.parse(localStorage.getItem("todosList") || "[]");
+}
 
-  todosWrapper.insertAdjacentHTML("beforeend", listItem);
+function saveTodoInLocalStorage(newTodo) {
+  const todosList = getTodosList();
+
+  return localStorage.setItem(
+    "todosList",
+    JSON.stringify([...todosList, newTodo])
+  );
+}
+
+function updateTodoInLocalStorage(newTodo) {
+  const todosList = getTodosList();
+
+  return localStorage.setItem(
+    "todosList",
+    JSON.stringify([...todosList, newTodo])
+  );
+}
+
+const updateTodoItem = (todoItem, checked, id) => {
+  if (checked) {
+    todoItem.classList.add("line-through");
+  } else {
+    todoItem.classList.remove("line-through");
+  }
+
+  const todosList = getTodosList();
+  const selectedTodoItem = todosList.find(
+    (currTodo) => parseInt(currTodo.id) === parseInt(id)
+  );
+
+  selectedTodoItem.isCompleted = checked;
+
+  const index = todosList.findIndex((todo) => todo.id === selectedTodoItem.id);
+
+  todosList[index] = selectedTodoItem;
+
+  localStorage.setItem("todosList", JSON.stringify([...todosList]));
+};
+
+const renderTodoItem = (newTodo, action = "display") => {
+  const { id, todo, isCompleted } = newTodo;
+
+  if (action === "create") {
+    // Add Todo to LocalStorage
+
+    saveTodoInLocalStorage(newTodo);
+    // updateTotalTodosLeft();
+  }
+
+  const li = document.createElement("li");
+  li.id = id;
+  li.className = "todo-item";
+
+  // ======== CHECKBOX WRAPPER ========
+  const checkboxWrapper = document.createElement("div");
+  checkboxWrapper.className = "rounded-corner";
+
+  // ======== CHECKBOX ========
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkbox";
+  checkbox.checked = isCompleted;
+  checkbox.addEventListener("change", function (event) {
+    const currElement = event.target;
+    const todoItem = currElement.parentElement.nextSibling;
+
+    updateTodoItem(todoItem, checkbox.checked, id);
+  });
+
+  // ======== CHECK ========
+  const check = document.createElement("span");
+  check.className = "check";
+
+  // ======== TODO ITEM ========
+  const paragraph = document.createElement("p");
+  paragraph.className = "todo";
+  paragraph.textContent = todo;
+  checkbox.checked ? paragraph.classList.add("line-through") : null;
+
+  // Append Checkbox and Check inside CheckboxWrapper
+  checkboxWrapper.appendChild(checkbox);
+  checkboxWrapper.appendChild(check);
+
+  // Appends Checkbox Wrapper and paragraph to li
+  li.appendChild(checkboxWrapper);
+  li.appendChild(paragraph);
+
+  // Append li to todosWrapper
+  todosWrapper.appendChild(li);
+
+  updateTotalTodosLeft();
+};
+
+const displayTodos = () => {
+  const todosList = getTodosList();
+
+  todosList.forEach((todoObj) => {
+    renderTodoItem(todoObj, "display");
+  });
+};
+
+const updateTotalTodosLeft = () => {
+  const todosList = getTodosList();
+  const totalNumberOfTodos = todosList.length;
+
+  numOfTodosLeft.textContent = `${totalNumberOfTodos} Items Left`;
+};
+
+const addNewTodo = (value) => {
+  const todosList = getTodosList();
+
+  const newTodo = {
+    id: todosList.length + 1,
+    todo: value,
+    isCompleted: false,
+  };
+
+  renderTodoItem(newTodo, "create");
 };
 
 const clearTodosInput = () => {
   todosInput.value = "";
 };
+
+displayTodos();
